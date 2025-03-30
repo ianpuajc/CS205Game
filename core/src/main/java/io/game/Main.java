009d526c37eac87fe2ed8d77bae2a94c4081a292
petercegoh <128559610+petercegoh@users.noreply.github.com>
2025-03-30 17:54:49 +0800

    ok now it works

--- core/src/main/java/io/game/Main.java
+++ core/src/main/java/io/game/Main.java
@@ -1,20 +1,14 @@
 package io.game;
 
-import com.badlogic.gdx.ApplicationAdapter;
+
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 public class Main extends Game {
     public SpriteBatch batch;
     public OrthographicCamera camera;
-    private Stage stage;
-    private GameLevel gameLevel;
-    private GameRenderer renderer;
-    private InputHandler inputHandler;
 
     @Override
     public void create() { // called on initial running
@@ -29,29 +23,11 @@ public class Main extends Game {
         //loadLevel(LevelLoader.getCurrentLevel());
         // i suppose this is the code that gets up th actual obstacle and what it looks like
         // but with menu screen, the menu screen should be created first, not all this.
-
-    }
-
-    private void loadLevel(int level) {
-        if (gameLevel != null) gameLevel.dispose();
-        gameLevel = LevelLoader.loadLevel(level);
-        renderer = new GameRenderer(batch, camera, gameLevel, stage);
-        inputHandler = new InputHandler(gameLevel, stage);
-        Gdx.input.setInputProcessor(stage);
     }
 
     @Override
-    public void render() { // runs continously automatically, at 60fps
-        float deltaTime = Gdx.graphics.getDeltaTime();
-        gameLevel.update(deltaTime);
-        inputHandler.update(deltaTime);
-        renderer.render();
-    }
-
-    @Override
-    public void dispose() { // when games ends
+    public void dispose() {
         batch.dispose();
-        gameLevel.dispose();
-        stage.dispose();
+        getScreen().dispose();
     }
 }
--- core/src/main/java/io/game/SimpleScreen.java
+++ core/src/main/java/io/game/SimpleScreen.java
@@ -1,4 +1,72 @@
 package io.game;
 
-public class SimpleScreen {
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+
+public class SimpleScreen implements Screen {
+    private Stage stage;
+    private Label label;
+    private Skin skin;
+
+    public SimpleScreen() {
+        stage = new Stage();
+        Gdx.input.setInputProcessor(stage);
+
+        // Load the skin (make sure to have the correct path for your skin files)
+        skin = new Skin(Gdx.files.internal("uiskin.json"));
+
+        // Create a label with a simple message
+        label = new Label("Hello, World!", new Label.LabelStyle(skin.getFont("default-font"), null));
+        label.setPosition(Gdx.graphics.getWidth() / 2f - label.getWidth() / 2f, Gdx.graphics.getHeight() / 2f - label.getHeight() / 2f);
+
+        // Add the label to the stage
+        stage.addActor(label);
+    }
+
+    @Override
+    public void show() {
+        // Called when this screen is set as the current screen
+    }
+
+    @Override
+    public void render(float delta) {
+        // Clear the screen
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        // Draw the stage (this will render the label)
+        stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
+        stage.draw();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        // Called when the screen is resized
+        stage.getViewport().update(width, height, true);
+    }
+
+    @Override
+    public void hide() {
+        // Called when this screen is no longer the current screen
+    }
+
+    @Override
+    public void pause() {
+        // Called when the game is paused
+    }
+
+    @Override
+    public void resume() {
+        // Called when the game is resumed
+    }
+
+    @Override
+    public void dispose() {
+        // Clean up resources when this screen is disposed of
+        stage.dispose();
+        skin.dispose();
+    }
 }
