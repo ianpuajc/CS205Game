1438af3b0eff036f3c33893d01ecc237fd46b5f8
tanialee21 <tl.gunawan.2023@scis.smu.edu.sg>
2025-04-18 23:32:15 +0800

    Edited Gameoveroverlay

--- core/src/main/java/io/game/GameOverOverlay.java
+++ core/src/main/java/io/game/GameOverOverlay.java
@@ -1,6 +1,7 @@
 package io.game;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Actor;
@@ -12,6 +13,7 @@ import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.utils.Align;
 
 public class GameOverOverlay {
     private final Table root;
@@ -19,33 +21,42 @@ public class GameOverOverlay {
     private final Stage stage;
     private final Main  game;
 
-
     GameOverOverlay(Stage stage, Main game) {
-        this.stage = stage; this.game = game;
+        this.stage = stage;
+        this.game = game;
         Skin skin = new Skin(Gdx.files.internal("uiskin.json"));
 
         root = new Table(skin);
         root.setFillParent(true);
         root.setVisible(false);
+        root.align(Align.center);
 
-        root.setBackground((Drawable) null);
+        Pixmap black = new Pixmap(1, 1, Pixmap.Format.RGBA8888);
+        black.setColor(0, 0, 0, 0.8f); // Solid black with alpha
+        black.fill();
+        TextureRegionDrawable solidBlack = new TextureRegionDrawable(new TextureRegion(new Texture(black)));
+        root.setBackground(solidBlack);
+
+        // Inner table for content
+        Table content = new Table(skin);
+        content.align(Align.center);
 
         Label title = new Label("GAME OVER", skin);
         scoreLabel = new Label("Score: 0", skin);
 
         TextButton menu = new TextButton("Main Menu", skin);
         menu.addListener(new ChangeListener() {
-            @Override public void changed(ChangeEvent e, Actor a) {
+            @Override
+            public void changed(ChangeEvent e, Actor a) {
                 game.setScreen(new GameMenuScreen(game));
             }
         });
 
-        root.add(title).pad(20);
-        root.row();
-        root.add(scoreLabel).pad(10);
-        root.row();
-        root.add(menu).size(200,80).pad(20);
+        content.add(title).padBottom(30).row();
+        content.add(scoreLabel).padBottom(30).row();
+        content.add(menu).width(250).height(60);
 
+        root.add(content);
         stage.addActor(root);
     }
 
